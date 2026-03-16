757171a2ab65134516edb1becdf43b9076d9bb00
Ojasvi Bhagat <ojasvibhagat012005@gmail.com>
2026-03-16 23:53:36 +0530

    Added recursion practice code

--- Recursion_level_1/src/Both_N_to_1_and_1_to_N.java
+++ Recursion_level_1/src/Both_N_to_1_and_1_to_N.java
@@ -1,2 +1,14 @@
 public class Both_N_to_1_and_1_to_N {
+    public static void main(String[] args) {
+        funBoth(5);
+
+    }
+    private static void funBoth(int n){
+        if(n==0){
+            return ;
+        }
+        System.out.println(n);
+        funBoth(n-1);
+        System.out.println(n);
+    }
 }
